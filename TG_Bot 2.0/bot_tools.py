308b3be2e683361631e756be4a445df668ce2621
wert1ges <117082369+wert1ges@users.noreply.github.com>
2023-04-01 12:19:39 +0300

    проверка оплаты
    добавил функцию проверки оплаты

--- TG_Bot 2.0/bot_tools.py
+++ TG_Bot 2.0/bot_tools.py
@@ -3,12 +3,14 @@ import bot
 
 
 def update_result():
-    con = sqlite3.connect("..DB/MainDB.db")
+    con = sqlite3.connect("../DB/MainDB.db")
     cur = con.cursor()
     ids = cur.execute("SELECT id FROM Rejected_requests").fetchall()
     for id0 in ids:
-        tg_id = cur.execute("SELECT tg_id FROM Students WHERE id = ?", (id0,))
-        bot.bot.send_message(tg_id, "Ваша перевод некорректен")
+        print(id0)
+        tg_id = cur.execute(f"SELECT tg_id FROM Students WHERE id = {id0[0]}").fetchone()
+        print(tg_id[0])
+        bot.bot.send_message(tg_id[0], "Ваша перевод некорректен")
 
 
 update_result()
