be835dcb10db6745feaba11887b670bdfa2bb0b1
Sasha-v-tanke <93387431+Sasha-v-tanke@users.noreply.github.com>
2023-04-04 19:55:00 +0300

    Revert "Squashed commit of the following:"
    This reverts commit d2801fea767da7715dd7d4d3482285739990a250.

--- QR_code_reader/main_qr_reader.py
+++ QR_code_reader/main_qr_reader.py
@@ -1,19 +1,11 @@
 import sys
-from json import loads
-
-from PIL import Image
-from PIL.ImageQt import ImageQt
+import cv2
 from PyQt5 import uic
 from PyQt5.QtCore import QTimer
-from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from imutils import resize
 from imutils.video import VideoStream
-from numpy import array
 from pyzbar import pyzbar
-
-from QR_code_reader import UI_FILE
-from QR_code_reader.modules import get_inf_from_bot
 
 
 class MainWindow(QMainWindow):
@@ -21,21 +13,12 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.previous_qr_code = ""
         self.camera_on = False
-        self.load_ui(UI_FILE)
-
+        self.load_ui('Support/main_window.ui')
         self.reset_status()
         self.start_camera()
 
-    def set_new_photo(self, pixmap: QPixmap):
-        w_old, h_old = pixmap.width(), pixmap.height()
-        w_max, h_max = self.image.width(), self.image.height()
-        k = max(w_old / w_max, h_old / h_max)
-        pixmap = pixmap.scaled(int(w_old / k), int(h_old / k))
-        self.image.setPixmap(pixmap)
-
     def load_ui(self, file_name: str):
         uic.loadUi(file_name, self)
-        self.showMaximized()
 
     def update(self, delta: int = 100):
         QTimer.singleShot(delta, self.update)
@@ -68,17 +51,8 @@ class MainWindow(QMainWindow):
             return False
 
         code, student_id = words
-        res = get_inf_from_bot(code, student_id)
-        if res["ok"]:
-            img = Image.fromarray(array(loads(res["image"]), dtype='uint8'))
-            qim = ImageQt(img)
-            pix = QPixmap.fromImage(qim)
-            ans = str(res["status"]), res["name"] + "   " + res["grade"], pix
-            return ans
-        else:
-            self.reset_status()
-            self.status_2.setText(res["description"])
-            return ["", "", QPixmap()]
+        res = self.get_inf_from_bot(code, student_id)
+        return res
 
     def start_camera(self):
         try:
@@ -92,10 +66,12 @@ class MainWindow(QMainWindow):
         self.camera_on = False
         self.vs.stop()
 
+    def get_inf_from_bot(self, code, student_id):
+        return ["0"] * 3
+
     def update_status(self, status, name, photo):
         self.about.setText(name)
-        self.status_2.setText(status)
-        self.set_new_photo(photo)
+        self.status.setText(status)
 
     def reset_status(self):
         self.about.setText("")
--- WebAPI/main.py
+++ WebAPI/main.py
@@ -1,59 +1,25 @@
-from json import dumps
-from numpy import array
 from bottle import route, request, run
 
-from WebAPI import HOST, PORT
-from WebAPI.modules import get_inf, check_code, get_photo_from_db
+from WebAPI.Support.variables import PORT, HOST
+from WebAPI.modules import get_inf_from_db
 
 
-@route('/get_information')
+@route('/get_inf')
 def get_information() -> dict:
     if not hasattr(request.query, "code"):
         return {"ok": False, "description": "неверный код"}
-    code = request.query.code
-    if not check_code(code):
-        return {"ok": False, "description": "неверный код"}
-
     if not hasattr(request.query, "id"):
         return {"ok": False, "description": "неверный id"}
     student_id = request.query.id
-
-    if not hasattr(request.query, "student_code"):
-        return {"ok": False, "description": "неверный id"}
-    student_code = request.query.student_code
-
-    res = get_inf(student_id, student_code)
-    if res:
-        status, name, grade, photo = res
-        return {"ok": True, "status": status, "name": name, "grade": grade, "image": photo}
-    return {"ok": False, "description": "ошибка во время получения информации из БД"}
+    inf = get_inf_from_db(student_id)
+    return inf
 
 
-@route('/check_code')
-def check() -> dict:
+@route('/check')
+def get_information() -> dict:
     if not hasattr(request.query, "code"):
         return {"ok": False, "description": "неверный код"}
-    code = request.query.code
-    if check_code(code):
-        return {"ok": True}
-    return {"ok": False, "description": "неверный код"}
-
-
-@route('/get_photo')
-def get_photo() -> dict:
-    if not hasattr(request.query, "code"):
-        return {"ok": False, "description": "неверный код"}
-    code = request.query.code
-    if not check_code(code):
-        return {"ok": False, "description": "неверный код"}
-
-    if not hasattr(request.query, "id"):
-        return {"ok": False, "description": "неверный id"}
-    student_id = request.query.id
-    image = get_photo_from_db(student_id)
-    image_data = dumps(array(image).tolist())
-    ans = {"ok": True, "image": image_data}
-    return ans
+    return {"ok": True}
 
 
 def main():
